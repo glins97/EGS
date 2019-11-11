a5fd365f6b3bb8bc6674bc17961e312a5e23a14f
Gabriel Lins <gabriel.lins97@gmail.com>
2019-11-11 15:57:12 -0300

    User now has access_level attribute

--- src/common/user.py
+++ src/common/user.py
@@ -1,10 +1,11 @@
 from .base_class import BaseClass
 
 class User(BaseClass):
-    def __init__(self, cpf, name, telephone, email, password):
+    def __init__(self, cpf, name, telephone, email, password, access_level):
         self.pks = ['cpf']
         self.cpf = cpf
         self.name = name
         self.telephone = telephone
         self.email = email
         self.password = password
+        self.access_level = access_level
--- utils/init_bd.py
+++ utils/init_bd.py
@@ -11,7 +11,7 @@ accounts = bd.load('accounts')
 lifts = bd.load('lifts')
 reservations = bd.load('reservations')
 
-user = User('111.111.111-11', 'Gabriel', '(61) 99964-0993', 'gabriel.lins97@gmail.com', '1234')
+user = User('111.111.111-11', 'Gabriel', '(61) 99964-0993', 'gabriel.lins97@gmail.com', '1234', 'admin')
 account = Account('BRB', '1111-1x', 141516)
 reservation = Reservation(1, 'Front2', False)
 lift = Lift(1, 'BSB', 'DF', 'RJ', 'RJ', str(datetime.datetime.now()), '60', 4, 159.9)
